692007c109cb4d9702642bc0b27af2555b396eca
Ssifon316 <Ssifon316@gmail.com>
2016-03-30 16:52:34 -0400

    Update knights_move.py

--- knights_move.py
+++ knights_move.py
@@ -1,3 +1,7 @@
+"""Problem #1: Knights Move on Keypad
+   Given a numeric keypad, chooing successive digits as a knight moves in chess, determine how many
+   different paths can be formed of lenght n."""
+
 knight ={0:[4,6], 1:[6,8], 2:[7,9], 3:[4,8], 4:[3,9,0],5:[], 6:[1,7,0], 7:[2,6], 8:[1,3], 9:[2,4]}
 
 def knights_move(n):
